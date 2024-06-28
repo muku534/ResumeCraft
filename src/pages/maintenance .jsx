f4981d450bbc10e56bf05ab72de2e9b7a5372701
muku534 <prajapatimukesh0111@gmail.com>
2024-06-28 12:25:41 +0530

    Add maintenance page and redirect rule

---
+++ src/pages/maintenance .jsx
@@ -0,0 +1,9 @@
+// pages/maintenance.js
+export default function Maintenance() {
+    return (
+        <div>
+            <h1>We'll be back soon!</h1>
+            <p>Our website is currently undergoing maintenance. Please check back later.</p>
+        </div>
+    );
+}
